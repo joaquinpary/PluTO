12d9c0abc0fe527da02a80d08867718df23b05a1
SantiagoColque <santiago.colque@mi.unc.edu.ar>
2026-05-13 18:11:31 -0300

    feat: add mqtt credentials

--- services/coord_transform/main.py
+++ services/coord_transform/main.py
@@ -9,8 +9,15 @@ def main():
     logger.info("Starting coordinates transformation engine...")
     broker_url = os.environ.get("MQTT_BROKER_URL", "mosquitto")
     broker_port = int(os.environ.get("MQTT_BROKER_PORT", "1883"))
+    broker_user = os.environ.get("MQTT_USERNAME", "pluto")
+    broker_pass = os.environ.get("MQTT_PASSWORD", "change-me")
     
-    mqtt_client = CoordinateTransformMQTTClient(broker_url=broker_url, broker_port=broker_port)
+    mqtt_client = CoordinateTransformMQTTClient(
+        broker_url=broker_url, 
+        broker_port=broker_port,
+        username=broker_user,
+        password=broker_pass
+    )
     try:
         mqtt_client.start()
     except KeyboardInterrupt:
--- services/coord_transform/mqtt_handler.py
+++ services/coord_transform/mqtt_handler.py
@@ -8,10 +8,12 @@ from transformer import transform_coordinates
 logger = logging.getLogger(__name__)
 
 class CoordinateTransformMQTTClient:
-    def __init__(self, broker_url="mosquitto", broker_port=1883):
+    def __init__(self, broker_url="mosquitto", broker_port=1883, username=None, password=None):
         self.broker_url = broker_url
         self.broker_port = broker_port
         self.client = mqtt.Client()
+        if username and password:
+            self.client.username_pw_set(username, password)
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
 
