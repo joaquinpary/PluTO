270f58d09d20e85ca8c8f1d0ae7e08719209f794
SantiagoColque <santiago.colque@mi.unc.edu.ar>
2026-05-13 17:45:09 -0300

    feat: add mqtt integration, and main loop

--- services/coord_transform/main.py
+++ services/coord_transform/main.py
@@ -1,11 +1,20 @@
 import logging
+import os
+from mqtt_handler import CoordinateTransformMQTTClient
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger("coord_transform_main")
 
 def main():
     logger.info("Starting coordinates transformation engine...")
-    # TODO: Inicializar conexión MQTT y mantener loop
+    broker_url = os.environ.get("MQTT_BROKER_URL", "mosquitto")
+    broker_port = int(os.environ.get("MQTT_BROKER_PORT", "1883"))
+    
+    mqtt_client = CoordinateTransformMQTTClient(broker_url=broker_url, broker_port=broker_port)
+    try:
+        mqtt_client.start()
+    except KeyboardInterrupt:
+        logger.info("Shutting down transformation engine...")
 
 if __name__ == "__main__":
     main()
